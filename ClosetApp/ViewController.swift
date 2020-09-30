6b0217ae92bb49d47dccfc3843c96aac81111f64
Miu Yamamoto <mi.oct09@gmail.com>
2020-09-30 16:39:03 +0900

    Initial Commit

---
+++ ClosetApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClosetApp
+//
+//  Created by Yamamoto Miu on 2020/09/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
