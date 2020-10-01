5da255571467c33142b5c0c04a5761060adcf35a
Miu Yamamoto <mi.oct09@gmail.com>
2020-10-02 03:48:06 +0900

    画像選択&filterの実装　(camera未検証, filter1種のみ)

--- ClosetApp/AddViewController.swift
+++ ClosetApp/AddViewController.swift
@@ -7,10 +7,14 @@
 
 import UIKit
 
-class AddViewController: UIViewController {
+class AddViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
     @IBOutlet weak var cameraImageView: UIImageView! //選択した画像を入れるためのImageViewを用意
     
+    //画像加工するための元となる画像
+    var originalImage: UIImage!
+    //画像加工するフィルターの宣言
+    var filter: CIFilter!
     
     
     override func viewDidLoad() {
@@ -30,14 +34,36 @@ class AddViewController: UIViewController {
             let takePhotoAction = UIAlertAction(title: "写真を撮る", style: UIAlertAction.Style.default, handler: {
                 (action:UIAlertAction!) in
                 //実際の処理
-                print("カメラ起動の処理")
+                if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                    //カメラを起動
+                    let picker = UIImagePickerController()
+                    picker.sourceType = .camera
+                    picker.delegate = self
+                    
+                    picker.allowsEditing = true
+                    
+                    self.present(picker, animated: true, completion: nil)
+                }else{
+                    //カメラが使えない時はエラーを表示
+                    print("error")
+                }
             })
             
             //2つ目のactionボタンが押されたときの処理
             let openLibraryAction = UIAlertAction(title: "ライブラリから選択", style: UIAlertAction.Style.default, handler: {
                 (action:UIAlertAction!) in
                 //実際の処理
-                print("ライブラリを開く処理")
+                if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+                    
+                    //カメラロールの画像を選択・表示
+                    let picker = UIImagePickerController()
+                    picker.sourceType = .photoLibrary
+                    picker.delegate = self
+                    
+                    picker.allowsEditing = true
+                    
+                    self.present(picker, animated: true, completion: nil)
+                }
             })
             
             // 閉じるボタンが押された時の処理をクロージャ実装する
@@ -56,5 +82,53 @@ class AddViewController: UIViewController {
             //実際にAlertを表示する
             self.present(actionSheet, animated: true, completion: nil)
         }
+    
+    //選択した画像をアプリ内に表示するためのメソッド
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        cameraImageView.image = info[.editedImage] as? UIImage
+        
+        //撮った画像を加工前のoriginalImageとして記憶しておく
+        originalImage = cameraImageView.image
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //フィルター加工action
+    @IBAction func filter1(_ sender: Any) {
+        colorFilter()
+    }
+    @IBAction func filter2(_ sender: Any) {
+        colorFilter()
+    }
+    @IBAction func filter3(_ sender: Any) {
+        colorFilter()
+    }
+    
+    
+    
+    //フィルター加工するためのメソッド
+    func colorFilter() {
+        let filterImage: CIImage = CIImage(image: originalImage)!
+        
+        //フィルターの設定
+        filter = CIFilter(name: "CIColorControls")!
+        filter.setValue(filterImage, forKey: kCIInputImageKey)
+        
+        //彩度
+        filter.setValue(1.0, forKey: "inputSaturation")
+        //明度
+        filter.setValue(0.5, forKey: "inputBrightness")
+        //コントラスト
+        filter.setValue(2.0, forKey: "inputContrast")
+        
+        let ctx = CIContext(options: nil)
+        let cgImage = ctx.createCGImage(filter.outputImage!, from: filter.outputImage!.extent)
+        cameraImageView.image = UIImage(cgImage: cgImage!)
+        
+        
+    }
+    
+    
 
 }
